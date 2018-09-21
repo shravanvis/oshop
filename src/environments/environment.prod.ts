706d6bc1a530dac9515b8aab62fca706f149d326
shravanvis <shravanvishwakarma5@gmail.com>
2018-09-21 18:51:06 +0530

    First commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+          apiKey: "AIzaSyDnBW5OCNFiiVjG3JUt8p7Q3vl5qij52yU",
+          authDomain: "oshop-b9206.firebaseapp.com",
+          databaseURL: "https://oshop-b9206.firebaseio.com",
+          projectId: "oshop-b9206",
+          storageBucket: "oshop-b9206.appspot.com",
+          messagingSenderId: "693177546373"
+            }       
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+          apiKey: "AIzaSyDnBW5OCNFiiVjG3JUt8p7Q3vl5qij52yU",
+          authDomain: "oshop-b9206.firebaseapp.com",
+          databaseURL: "https://oshop-b9206.firebaseio.com",
+          projectId: "oshop-b9206",
+          storageBucket: "oshop-b9206.appspot.com",
+          messagingSenderId: "693177546373"
+           }
 };
 
 /*
